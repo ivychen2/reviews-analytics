d0585ca78d56c8fcc4f7964ea091fbe2d2daa3da
ivy Chen <ivy.chen2@gmail.com>
2021-05-06 15:58:40 +0800

    add new filter

--- read.py
+++ read.py
@@ -15,3 +15,11 @@ for d in data:
 
 avg_len = sum_len / len(data)
 print('留言平均長度為： ', avg_len)
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
+print(new[0])
+print(new[1])
